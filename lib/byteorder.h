144a5fceaf041e5baf55b3dde1bf88f609cc1b66
Andreas Gruenbacher <andreas.gruenbacher@gmail.com>
2015-10-21 21:55:51 +0200

    Switch to the kernel-provided endian conversion macros

--- lib/byteorder.h
+++ lib/byteorder.h
@@ -1,38 +1,29 @@
 /*
-  Copyright (C) 2000, 2002  Novell, Inc.
-  Written by Andreas Gruenbacher <agruen@suse.de>
+  Copyright (C) 2015  Red Hat, Inc.
+  Written by Andreas Gruenbacher <agruenba@redhat.com>
 
-  This program is free software: you can redistribute it and/or modify it
-  under the terms of the GNU Lesser General Public License as published by
-  the Free Software Foundation, either version 2.1 of the License, or
-  (at your option) any later version.
+  The richacl library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation; either version 2.1 of the
+  License, or (at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
+  The richacl library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU Lesser General Public License for more details.
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, see
+  <http://www.gnu.org/licenses/>.
 */
 
-#include <endian.h>
+#include <linux/types.h>
+#include <asm/byteorder.h>
 
-#if __BYTE_ORDER == __BIG_ENDIAN
-# define cpu_to_le16(w16) le16_to_cpu(w16)
-# define le16_to_cpu(w16) ((u_int16_t)((u_int16_t)(w16) >> 8) | \
-                           (u_int16_t)((u_int16_t)(w16) << 8))
-# define cpu_to_le32(w32) le32_to_cpu(w32)
-# define le32_to_cpu(w32) ((u_int32_t)( (u_int32_t)(w32) >>24) | \
-                           (u_int32_t)(((u_int32_t)(w32) >> 8) & 0xFF00) | \
-                           (u_int32_t)(((u_int32_t)(w32) << 8) & 0xFF0000) | \
-			   (u_int32_t)( (u_int32_t)(w32) <<24))
-#elif __BYTE_ORDER == __LITTLE_ENDIAN
-# define cpu_to_le16(w16) ((u_int16_t)(w16))
-# define le16_to_cpu(w16) ((u_int16_t)(w16))
-# define cpu_to_le32(w32) ((u_int32_t)(w32))
-# define le32_to_cpu(w32) ((u_int32_t)(w32))
-#else
-# error unknown endianess?
-#endif
+#define le32 __le32
+#define le16 __le16
 
+#define cpu_to_le32 __cpu_to_le32
+#define cpu_to_le16 __cpu_to_le16
+#define le32_to_cpu __le32_to_cpu
+#define le16_to_cpu __le16_to_cpu
